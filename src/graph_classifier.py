28150f0ae0b410869177045fcae72772b6ca4255
Juanga Ricci <riccijuanga@gmail.com>
2019-08-13 09:54:06 -0300

    strcture and matplotlib

---
+++ src/graph_classifier.py
@@ -0,0 +1,13 @@
+""" This should able to analyse
+a graph and classify it as:
+- Vertex quantity
+- Edge quantity
+- If its directed or not
+- If its reflexive or not
+- If its transitive or not
+- If its disperse or dense
+- simple directed, simple undirected, multi
+- ...
+"""
+def classify_graph(graph):
+    pass
---
+++ src/matrixes.py
@@ -0,0 +1,9 @@
+"""
+methods to get matrix from a graph
+"""
+
+def adjacency_matrix(graph):
+    pass
+
+def incidency_matrix(graph):
+    pass
