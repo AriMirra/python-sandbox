c949615c291248cf1a0c117612eb520b45fdf6e6
arielmirra <ariel.mirra@ing.austral.edu.ar>
2019-08-20 11:07:14 -0300

    reformat my-graph class

--- src/MyGraph.py
+++ src/MyGraph.py
@@ -1,40 +1,50 @@
-from Graph import Graph
+from src.Graph import Graph
+
 """
 Graph class implementation
 """
 
+
 class MyGraph(Graph):
+
+    def get_adjacent_list(self, v):
+        pass
 
     def __init__(self):
         self.graph = {}
 
     def add_vertex(self, v):
-        if v is None: return False
+        if v is None:
+            return False
         self.graph[v] = []
         return True
 
     def add_edge(self, v, w):
-        if v is None or w is None: return False
+        if v is None or w is None:
+            return False
         self.graph[v].append(w)
         return True
 
     def delete_vertex(self, v):
         if v is None: return False
-        if self.graph.get(c) is None: return False
+        if self.graph.get(v) is None:
+            return False
         self.graph.pop(v)
         return True
 
     def delete_edge(self, v, w):
-        if v is None or w is None: return False
-        if self.graph.get(v) is None: return False
+        if v is None or w is None:
+            return False
+        if self.graph.get(v) is None:
+            return False
         self.graph[v].remove(w)
         return False
 
     def edge_exists(self, v, w):
         if v is None or w is None: raise TypeError("argument is None")
-        if self.graph.get(v) is None or self.graph.get(w) is None: 
+        if self.graph.get(v) is None or self.graph.get(w) is None:
             raise ValueError("no such element in v")
-        
+
         return True
 
     def order(self):
@@ -53,7 +63,7 @@ class MyGraph(Graph):
         return list(self.graph.keys())[i]
 
     def get_vertex_edges_list(self, v):
-        if v is None: raise TypeError("argument is None") 
+        if v is None: raise TypeError("argument is None")
         return self.graph[v]
 
     def get_vertexes_list(self):
