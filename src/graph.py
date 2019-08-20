3494dcaebb17c032eb66388f5d1ff67cb2335177
Juanga Ricci <riccijuanga@gmail.com>
2019-08-20 09:42:30 -0300

    some graph implementation

---
+++ src/graph.py
@@ -0,0 +1,60 @@
+import src.Graph
+"""
+Graph class implementation
+"""
+
+class MyGraph(Graph):
+
+    def __init__(self):
+        self.graph = {}
+
+    def add_vertex(self, v):
+        if v is None: return False
+        self.graph[v] = []
+        return True
+
+    def add_edge(self, v, w):
+        if v is None or w is None: return False
+        self.graph[v].append(w)
+        return True
+
+    def delete_vertex(self, v):
+        if v is None: return False
+        if self.graph.get(c) is None: return False
+        self.graph.pop(v)
+        return True
+
+    def delete_edge(self, v, w):
+        if v is None or w is None: return False
+        if self.graph.get(v) is None: return False
+        self.graph[v].remove(w)
+        return False
+
+    def edge_exists(self, v, w):
+        if v is None or w is None: raise TypeError("argument is None")
+        if self.graph.get(v) is None or self.graph.get(w) is None: 
+            raise ValueError("no such element in v")
+        
+        return True
+
+    def order(self):
+        return len(self.graph)
+
+    def edge_amount(self):
+        amount = 0
+        for edge in self.graph.values():
+            amount += len(edge)
+        return amount
+
+    def get_vertex(self, i):
+        if i is None: raise TypeError("argument is None")
+        if i < 0: raise ValueError("invalid (negative) value")
+        if i > len(self.graph) - 1: raise IndexError("no such element in index given")
+        return list(self.graph.keys())[i]
+
+    def get_vertex_edges_list(self, v):
+        if v is None: raise TypeError("argument is None") 
+        return self.graph[v]
+
+    def get_vertexes_list(self):
+        return list(self.graph.keys())
--- src/matrixes.py
+++ src/matrixes.py
@@ -1,9 +1,72 @@
 """
-methods to get matrix from a graph
+==== Matrixes functions and other theory =====================
+
+Graph has a dict and operates with it, the keys are
+vertexes and the values are lists with all connections
+with other vertexes.
+    Graph1 = {"A": ["B", "C"], "B": ["A"], "C": ["B"]}
+
+==== Super drawing ===========================================
+    (A)--<>--(B)     // example from above
+     |       A
+     |     /
+     |   /
+     V / 
+    (C)
+
+==== Directed and non directed ===============================
+If A has connection with B but not otherwise, the connection
+will be directed in one side, if both have reciprocal
+connection between each other, will be non directed.
+
 """
+from graph import MyGraph
 
 def adjacency_matrix(graph):
-    pass
+    matrix = []
+    vertexes = graph.get_vertex_list()
+    for i in range(0, len(vertexes)):
+        row = []
+        for j in range(0, len(vertexes)):
+            try:
+                if (graph.get_vertex_edge_list(vertexes[i])[j] == vertexes[i]):
+                    row.append(1)
+                else:
+                    row.append(0)
+            except IndexError:
+                row.append(0)
 
 def incidency_matrix(graph):
+    # exists but not really important
     pass
+
+# matrix test
+if __name__ == "__main__":
+    """ A --> B ---> C 
+        (demo_graph_1)
+
+    """
+    demo_graph_1 = MyGraph()
+    demo_graph_1.add_vertex("A")
+    demo_graph_1.add_vertex("B")
+    demo_graph_1.add_vertex("C")
+    demo_graph_1.add_edge("A", "B")
+    demo_graph_1.add_edge("B", "C")
+    """ E ----- F   (demo_graph_2)
+        |  ____/
+        | / 
+        G ----- H
+    """
+    demo_graph_2 = MyGraph()
+    demo_graph_2.add_vertex("E")
+    demo_graph_2.add_vertex("F")
+    demo_graph_2.add_vertex("G")
+    demo_graph_2.add_vertex("H")
+    demo_graph_2.add_edge("E", "F")
+    demo_graph_2.add_edge("F", "E")
+    demo_graph_2.add_edge("E", "G")
+    demo_graph_2.add_edge("G", "E")
+    demo_graph_2.add_edge("F", "G")
+    demo_graph_2.add_edge("G", "F")
+    demo_graph_2.add_edge("G", "H")
+    demo_graph_2.add_edge("H", "G")
