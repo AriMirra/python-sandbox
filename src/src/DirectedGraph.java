3ed975cc3f6e4f4359a022cafcaf77e12195bc6c
Ariel Mirra <ariel.mirra@ing.austral.edu.ar>
2019-11-03 20:54:31 -0300

    BFS & DFS

--- src/src/DirectedGraph.java
+++ src/src/DirectedGraph.java
@@ -1,5 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class DirectedGraph implements Graph {
@@ -37,10 +36,60 @@ public class DirectedGraph implements Graph {
 
     public List<Integer> getAdjacentList(int v) {
         ArrayList<Integer> result = new ArrayList<>(vertexes.size());
-        for (Edge e: edges) {
+        for (Edge e : edges) {
             if (e.from == v) result.add(e.to);
         }
         return result;
+    }
+
+    public boolean plainSearch(String from, String to) {
+        // TODO: ask Alicia
+        int fromIndex = vertexes.indexOf(from);
+        int toIndex = vertexes.indexOf(to);
+        for (Edge e: edges) {
+
+        }
+        return false;
+    }
+
+    public boolean DFS(String from, String to) {
+        HashSet<Integer> visited = new HashSet<>();
+        int fromIndex = vertexes.indexOf(from);
+        int toIndex = vertexes.indexOf(to);
+        return hasPathDFS(fromIndex, toIndex, visited);
+    }
+
+    private boolean hasPathDFS(int from, int to, HashSet<Integer> visited) {
+        if (visited.contains(from)) return false;
+        if (from == to) return true;
+        visited.add(from);
+        Set<Integer> children = getChildren(from);
+        for (int v : children) {
+            hasPathDFS(v, to, visited);
+        }
+        return false;
+    }
+
+    public boolean BFS(String from, String to) {
+        LinkedList<Integer> nextToVisit = new LinkedList<>();
+        HashSet<Integer> visited = new HashSet<>();
+        int fromIndex = vertexes.indexOf(from);
+        nextToVisit.add(fromIndex);
+        while(!nextToVisit.isEmpty()) {
+            int v = nextToVisit.remove();
+            if (vertexes.get(v).equals(to)) return true;
+            if (visited.contains(v)) continue;
+            visited.add(v);
+            nextToVisit.addAll(getChildren(v));
+        }
+        return false;
+    }
+
+    private Set<Integer> getChildren(int i) {
+        return edges.stream()
+                .filter(e -> e.from == i)
+                .map(e -> e.to)
+                .collect(Collectors.toSet());
     }
 
 }
